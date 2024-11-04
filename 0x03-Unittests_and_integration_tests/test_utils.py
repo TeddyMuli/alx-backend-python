324515d7105384faf13e1ea64dfa66e06c38ae33
TeddyMuli <tmuli974@gmail.com>
2024-11-04 23:28:38 +0300

    test get_json

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,8 +4,10 @@ unittests to test the utils module
 """
 #!/usr/bin/env python3
 from unittest import TestCase, main
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+
 
 
 class TestAccessNestedMap(TestCase):
@@ -31,6 +33,25 @@ class TestAccessNestedMap(TestCase):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
         self.assertEqual(str(context.exception), f"'{path[-1]}'")
+
+class TestGetJson(TestCase):
+    """ Test getjson
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """ Method to test get_json
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        result = get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
 
 if __name__ == "__main__":
     main()
