467a28fb7def477ff7ba9f144d4647ba879fff66
TeddyMuli <tmuli974@gmail.com>
2025-01-15 12:25:02 +0300

    fix pylint errors

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -6,12 +6,14 @@ async_comprehension four times in parallel using asyncio.gather.
 measure_runtime should measure the total runtime and return it.
 Notice that the total runtime is roughly 10 seconds, explain it to yourself.
 """
-import time, asyncio
-
-
+import time
+import asyncio
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
-async def measure_runtime():
+
+async def measure_runtime() -> float:
+    """ Measure runtime
+    """
     start = time.time()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end = time.time()
