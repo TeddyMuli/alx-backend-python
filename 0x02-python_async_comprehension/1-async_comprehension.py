e3d42e7b2f724c1a59c4c2c884c144840538bc41
TeddyMuli <tmuli974@gmail.com>
2025-01-15 11:18:03 +0300

    fix pylint errors

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -14,6 +14,7 @@ async def async_comprehension() -> List[float]:
     Import async_generator from the previous task and then
     write a coroutine called async_comprehension that takes no arguments.
     The coroutine will collect 10 random numbers using
-    an async comprehensing over async_generator, then return the 10 random numbers.
+    an async comprehensing over async_generator,
+    then return the 10 random numbers.
     """
     return [number async for number in async_generator()]
